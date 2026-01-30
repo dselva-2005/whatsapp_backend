11f088c519b94cd797756b3ac07e9d900edc6288
dselva-2005 <dselvajagan@gmail.com>
2026-01-30 08:57:45 +0530

    product images added one left to add

--- app/handlers/webhook.py
+++ app/handlers/webhook.py
@@ -27,19 +27,19 @@ PRODUCTS = {
         "preview_image": "https://allspray.in/static/images/product1.png",
         "code_image": "https://allspray.in/static/images/final_network.png",
         "original": 499,
-        "discount": 20,
+        "discount": 479,
     },
     "opt_2": {
         "preview_image": "https://allspray.in/static/images/product2.png",
         "code_image": "https://allspray.in/static/images/code2.png",
         "original": 699,
-        "discount": 20,
+        "discount": 679,
     },
     "opt_3": {
         "preview_image": "https://allspray.in/static/images/product3.png",
         "code_image": "https://allspray.in/static/images/code3.png",
         "original": 599,
-        "discount": 49,
+        "discount": 550,
     },
     "opt_4": {
         "preview_image": "https://allspray.in/static/images/product4.png",
