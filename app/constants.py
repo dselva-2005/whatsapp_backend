ddafdf145b7820c3663bf4cd5d2a03ef084a41eb
dselva-2005 <dselvajagan@gmail.com>
2026-02-04 20:32:10 +0530

    updated flow

--- app/constants.py
+++ app/constants.py
@@ -6,6 +6,6 @@
 
 PRODUCT = {
     "name": "Khalifa Hitech Mobile Offer",
-    "preview_image": "https://yourdomain.com/static/images/product1.png",
-    "code_image": "https://yourdomain.com/static/images/final network.png",
+    "preview_image": "https://allspray.in/static/images/product1.png",
+    "code_image": "https://allspray.in/static/images/final_network.png",
 }
